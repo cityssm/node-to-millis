22f12efcc0486513448e82f2e99fd3b123b5d1d9
Dan Gowans <dangowans@users.noreply.github.com>
2026-04-01 14:56:38 -0400

    update dev dependencies

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,10 @@
 import eslintConfigCityssm, {
-  type Config,
-  cspellWords,
-  tseslint
+  defineConfig,
+  type Config
 } from 'eslint-config-cityssm'
+import { cspellWords } from 'eslint-config-cityssm/exports.js'
 
-const config = tseslint.config(...eslintConfigCityssm, {
+const config = defineConfig(...eslintConfigCityssm, {
   files: ['**/*.ts'],
   rules: {
     '@cspell/spellchecker': [
